32a65e324db6920f0f9cf864bac1f2f3198a3c67
Shad0wAVM <arturvasco.martins@gmail.com>
2022-10-17 22:12:14 +0100

    DoRegisterClient

--- prr/app/client/DoRegisterClient.java
+++ prr/app/client/DoRegisterClient.java
@@ -14,10 +14,17 @@ class DoRegisterClient extends Command<Network> {
   DoRegisterClient(Network receiver) {
     super(Label.REGISTER_CLIENT, receiver);
     //FIXME add command fields
+    addStringField("key", Message.key());
+    addStringField("name", Message.name());
+    addIntegerField("NIF", Message.taxId());
+
   }
   
   @Override
   protected final void execute() throws CommandException {
-    //FIXME implement command
+    String key = stringField("key");
+    String name = stringField("name");
+    Integer taxNumber = integerField("NIF");
+    _receiver.registerClient(key, name, taxNumber);
   }
 }
