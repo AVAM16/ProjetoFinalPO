3c94336dd1b35e8619fd14f8d4a1ed68a1ed033e
Shad0wAVM <arturvasco.martins@gmail.com>
2022-10-18 09:47:46 +0100

    DoShowClient concluido e arranjado

--- prr/core/Client.java
+++ prr/core/Client.java
@@ -1,5 +1,6 @@
 package prr.core;
 
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -10,104 +11,105 @@ import prr.core.TariffPlan;
 import prr.core.Terminal;
 
 public class Client {
-    private String _key;
-    private String _name;
-    private int _nif;
-    private ClientLevel _level;
-    private boolean _receiveNotifications;
-    private List<Communication> _communicationsReceived;
-    private List<Communication> _communicationsMade;
-    private List<Communication> _communicationsPaid;
-    private List<Communication> _communicationsDept;
-    private List<Notifications> _notifications;
-    private TariffPlan _plan;
-    private List<Terminal> _terminals;
+  private String _key;
+  private String _name;
+  private int _nif;
+  private ClientLevel _level;
+  private boolean _receiveNotifications;
+  private List<Communication> _communicationsReceived;
+  private List<Communication> _communicationsMade;
+  private List<Communication> _communicationsPaid;
+  private List<Communication> _communicationsDept;
+  private List<Notifications> _notifications;
+  private TariffPlan _plan;
+  private List<Terminal> _terminals;
 
-    public Client(String key, String name, int nif){
-        this._key=key;
-        this._name=name;
-        this._nif=nif;
-        this._level = ClientLevel.NORMAL;
-        this._terminals= new LinkedList<>();
-        this._communicationsDept = new LinkedList<>();
-        this._communicationsPaid = new LinkedList<>();
-        this._communicationsReceived = new LinkedList<>();
-        this._communicationsMade = new LinkedList<>();
-    }
+  public Client(String key, String name, int nif){
+    this._key=key;
+    this._name=name;
+    this._nif=nif;
+    this._level = ClientLevel.NORMAL;
+    this._terminals= new LinkedList<>();
+    this._communicationsDept = new LinkedList<>();
+    this._communicationsPaid = new LinkedList<>();
+    this._communicationsReceived = new LinkedList<>();
+    this._communicationsMade = new LinkedList<>();
+  }
 
     //gets
 
-    public String getKey(){
-        return this._key;
+  public String getKey(){
+    return this._key;
+  }
+
+  public String getName(){
+    return this._name;
+  }
+
+  public int getNIF(){
+    return this._nif;
+  }
+
+  public ClientLevel getClientLevel(){
+    return this._level;
+  }
+
+  public double getDepts(){
+    double sum=0;
+    Iterator<Communication> iter = _communicationsDept.iterator();
+    while(iter.hasNext()){
+      Communication communication = iter.next();
+      sum = sum + communication.getCost();
     }
+    return sum;
+  }
 
-    public String getName(){
-        return this._name;
+  public double getPayments(){
+    double sum=0;
+    Iterator<Communication> iter = _communicationsPaid.iterator();
+    while(iter.hasNext()){
+      Communication communication = iter.next();
+      sum = sum + communication.getCost();
     }
+    return sum;
+  }
 
-    public int getNIF(){
-        return this._nif;
+  public int getTerminals(){
+    return _terminals.size();
+  }
+
+  public String getNotificationsString(){
+    if(_receiveNotifications==true){
+      return "YES";
     }
-
-    public ClientLevel getClientLevel(){
-        return this._level;
+    else{
+      return "NO";
     }
+  }
 
-    public double getDepts(){
-        double sum=0;
-        Iterator<Communication> iter = _communicationsDept.iterator();
-        while(iter.hasNext()){
-            Communication communication = iter.next();
-            sum = sum + communication.getCost();
-        }
-        return sum;
-    }
+  public List<Notifications> getNotifications(){
+    return _notifications;
+  }
 
-    public double getPayments(){
-        double sum=0;
-        Iterator<Communication> iter = _communicationsPaid.iterator();
-        while(iter.hasNext()){
-            Communication communication = iter.next();
-            sum = sum + communication.getCost();
-        }
-        return sum;
-    }
+  //adds
+  public void addTerminal(Terminal terminal){
+    this._terminals.add(terminal);
+  }
 
-    public int getTerminals(){
-        return _terminals.size();
-    }
+  public void addNotification(Communication communication,Client client,NotificationType type){
+    Notifications notification = new Notifications(communication, client, type);
+    _notifications.add(notification);
+  }
 
-    public String getNotificationsString(){
-        if(_receiveNotifications==true){
-            return "YES";
-        }
-        else{
-            return "NO";
-        }
-    }
+  public void clearNotifications(){
+    _notifications.clear();
+  }
 
-
-    //adds
-    public void addTerminal(Terminal terminal){
-        this._terminals.add(terminal);
-    }
-
-    public void addNotification(Communication communication,Client client,NotificationType type){
-        Notifications notification = new Notifications(communication, client, type);
-        _notifications.add(notification);
-    }
-
-
-
-    //CLIENT|key|name|taxId|type|notifications|terminals|payments|debts
-    // tipo-de-notificação|idTerminal
-    public void showClient(){
-        System.out.println(String.format("CLIENT|%d|%d|%d|%d|%d|%d|%d|%d",_key,_name,_nif,_level,getNotificationsString(),
-        getTerminals(),getPayments(),getDepts()));
-        for(Notifications notification : _notifications){
-            System.out.println(String.format("%d|%d",notification.getType().toString(),notification.getTeminalOrigin()));
-        }
-        _notifications.clear();
-    }
+  //CLIENT|key|name|taxId|type|notifications|terminals|payments|debts
+  // tipo-de-notificação|idTerminal
+  public String showClient(){
+    return(String.format("CLIENT|%d|%d|%d|%d|%d|%d|%d|%d",_key,_name,_nif,_level,getNotificationsString(),
+    getTerminals(),getPayments(),getDepts()));
+  }
     
 }
