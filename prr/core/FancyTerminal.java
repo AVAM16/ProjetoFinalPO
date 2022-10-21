ef94fc44edd2ecb9117edbd9cc39e0b6c6a0c898
goncalonovaiscruz <novaiscruzgoncalo@gmail.com>
2022-10-21 09:54:40 +0100

    pequena correcao

--- prr/core/FancyTerminal.java
+++ prr/core/FancyTerminal.java
@@ -8,11 +8,6 @@ public class FancyTerminal extends Terminal{
 
     //terminalType|terminalId|clientId|terminalStatus|balance-paid|balance-debts|friend1,...,friend
     public String showTerminal(){
-      if(getClient()==null){
-      String terminal = String.format("FANCY|%s|%s|%s|%d|%d%s",getID(),"NONE",getModeDisplay(),
-        getValue(getPayments()),getValue(getDepts()),getFriends());
-        return terminal;
-      }
       String terminal = String.format("FANCY|%s|%s|%s|%d|%d%s",getID(),getClient().getKey(),getModeDisplay(),
       getValue(getPayments()),getValue(getDepts()),getFriends());
 
