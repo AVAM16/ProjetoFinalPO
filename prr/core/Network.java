289a3c4c48136f61f33be28ef36acd530c1bea03
Artur <arturvasco.martins@gmail.com>
2022-10-26 14:44:42 +0100

    Revert "Pequeno arranjo"
    This reverts commit c93bc3d8905dbf627d96402104313c0211d5a410.

--- prr/core/Network.java
+++ prr/core/Network.java
@@ -130,10 +130,6 @@ public class Network implements Serializable {
 
   public Terminal registerTerminal(String type,String id,String clientID) throws InvalidTerminalKeyException, UnknownClientKeyException, DuplicateTerminalKeyException{
 
-    if(id.length() != 6 || !onlyDigits(id,6)){
-      throw new InvalidTerminalKeyException(id);
-    } 
-
     Iterator<Terminal> iter = _terminals.iterator();
     while(iter.hasNext()){
       Terminal terminal = iter.next();
@@ -142,6 +138,10 @@ public class Network implements Serializable {
       }
     }
     
+    if(id.length() != 6 || !onlyDigits(id,6)){
+      throw new InvalidTerminalKeyException(id);
+
+    }
     Terminal terminalNovo = null;
     Client client = findClient(clientID);
     
