a4f8095cae4dfe7b6f3b90011ee0590d953f10b0
Shad0wAVM <arturvasco.martins@gmail.com>
2022-11-01 10:04:14 +0000

    Client App concluida e DoShowGlobalBalance feito

--- prr/app/client/DoEnableClientNotifications.java
+++ prr/app/client/DoEnableClientNotifications.java
@@ -1,5 +1,6 @@
 package prr.app.client;
 
+import prr.core.Client;
 import prr.core.Network;
 import prr.app.exception.UnknownClientKeyException;
 import pt.tecnico.uilib.menus.Command;
@@ -14,10 +15,16 @@ class DoEnableClientNotifications extends Command<Network> {
   DoEnableClientNotifications(Network receiver) {
     super(Label.ENABLE_CLIENT_NOTIFICATIONS, receiver);
     //FIXME add command fields
+    addStringField("id", Message.key());
   }
   
   @Override
   protected final void execute() throws CommandException {
     //FIXME implement command
+    String id = stringField("id");
+    Client client = _receiver.findClient(id);
+    if (!client.enableNotifications()) {
+      _display.popup(Message.clientNotificationsAlreadyEnabled());
+    }
   }
 }
